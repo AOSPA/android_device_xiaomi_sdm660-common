86728f21edf638847e1872365c33764be09d4707
Stylogey <stylogey@gmail.com>
2020-05-08 11:49:01 +0200

    sdm660-common: use common wfd blobs
    Change-Id: I981618f7b08dae7bd308e9a7f9e330c3ea88034a

--- sdm660.mk
+++ sdm660.mk
@@ -185,10 +185,7 @@ PRODUCT_PACKAGES += \
     gralloc.sdm660 \
     hwcomposer.sdm660 \
     memtrack.sdm660 \
-    libdisplayconfig \
     liboverlay \
-    libqdMetaData \
-    libqdMetaData.system \
     libtinyxml \
     vendor.display.config@1.9 \
     vendor.display.config@1.9_vendor
@@ -425,7 +422,8 @@ PRODUCT_COPY_FILES += \
 # QTI common
 TARGET_COMMON_QTI_COMPONENTS := \
     perf \
-    bt
+    bt \
+    wfd-legacy
 
 # RCS
 PRODUCT_PACKAGES += \
@@ -531,12 +529,3 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(COMMON_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(COMMON_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
-
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libaacwrapper \
-    libnl
-
-# WiFi Display JAR
-PRODUCT_BOOT_JARS += \
-    WfdCommon
