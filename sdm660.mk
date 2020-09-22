5da5c4a77a11f1c7b6aa021a109153442868f016
jimmy <quesoo@gmail.com>
2020-09-22 21:38:21 +0200

    sdm660-common: clean up vndk packages
    Change-Id: I6134b2065436a53dbdedcc06b82bfd208860d7c3

--- sdm660.mk
+++ sdm660.mk
@@ -366,7 +366,7 @@ PRODUCT_PACKAGES += \
 # VNDK
 PRODUCT_PACKAGES += \
     libdng_sdk.vendor_32 \
-    libstdc++.vendor_32  \
+    libstdc++.vendor  \
     vndk_package \
     vndk-ext
 
--- vndk/vndk-ext-libs.mk
+++ vndk/vndk-ext-libs.mk
@@ -1,20 +1,4 @@
-#
-# Copyright (C) 2019 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-EXTRA_VENDOR_LIBRARIES_32 := \
+EXTRA_VENDOR_LIBRARIES := \
     libandroidicu \
     libaudioclient \
     libaudiomanager \
@@ -24,6 +8,7 @@ EXTRA_VENDOR_LIBRARIES_32 := \
     libbufferhub \
     libbufferhubqueue \
     libcamera_client \
+    libcutils \
     libft2 \
     libgui \
     libharfbuzz_ng \
@@ -39,12 +24,7 @@ EXTRA_VENDOR_LIBRARIES_32 := \
     libnblog \
     libpackagelistparser \
     libpdx_default_transport \
+    libpowermanager \
     libselinux \
-    libstdc++ \
     libsonivox \
     libvibrator
-
-EXTRA_VENDOR_LIBRARIES_64 := \
-    libicuuc \
-    libstdc++ \
-    libpowermanager
