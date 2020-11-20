3cc6bfd6c4e185e563b2cf75635a2378e402653f
Ethan Chen <intervigil@gmail.com>
2020-11-20 11:15:41 +0530

    sdm660-common: Add AOSP RCS packages
    Change-Id: Id1650cc499360e4003dba52b8615b4b4a9957fd4

--- sdm660.mk
+++ sdm660.mk
@@ -400,6 +400,12 @@ TARGET_COMMON_QTI_COMPONENTS := \
     bt \
     wfd-legacy
 
+# RCS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    PresencePolling \
+    RcsService
+
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
