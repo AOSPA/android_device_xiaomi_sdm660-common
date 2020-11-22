b56b5ba2c30774e9e35403e6e4fda4dd50616988
Jake Weinstein <jake@aospa.co>
2020-11-22 20:54:49 +0100

    sdm660-common: match CAF LA.UM.9.2 graphics properties
    Change-Id: I5e727e2e322b74e82013a4d0072a369c24071fe9

--- common_prop.mk
+++ common_prop.mk
@@ -132,14 +132,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.latch_unsignaled=1 \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
-    debug.sf.disable_backpressure= 1 \
-    debug.sf.early_app_phase_offset_ns=1500000 \
-    debug.sf.early_gl_phase_offset_ns=3000000 \
-    debug.sf.early_gl_app_phase_offset_ns=15000000 \
-    debug.sf.early_phase_offset_ns=1500000 \
-    debug.sf.enable_gl_backpressure=1 \
-    debug.cpurend.vsync=false \
-    debug.hwui.use_buffer_age=false \
     persist.hwc.enable_vds=1 \
     ro.opengles.version=196610 \
     ro.qualcomm.cabl=0 \
@@ -268,9 +260,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
-    ro.surface_flinger.protected_contents=true \
-    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
+    ro.surface_flinger.protected_contents=true
 
 #Storaged
  PRODUCT_PROPERTY_OVERRIDES += \
